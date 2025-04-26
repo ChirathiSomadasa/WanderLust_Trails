1a5a01e1ee18a3ab8f35cceccea901cbb64a7327
ChirathiSomadasa <chirathisomadasa@gmail.com>
2025-04-26 16:03:29 +0530

    Revert "Update server.js"
    This reverts commit 5cbc5230e122437fb77d64576f1fba97c5ed56d5.

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors({ origin: "https://wander-lust-trails-ia8n.vercel.app/", credentials: true }));
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
