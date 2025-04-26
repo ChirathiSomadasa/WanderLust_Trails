d0d8ebb10b54591e6ae19b2127dadfc38d3c5c26
ChirathiSomadasa <chirathisomadasa@gmail.com>
2025-04-26 16:03:19 +0530

    Revert "update server.js and .env"
    This reverts commit d63eae0ff3fdcd77eea9c3a8742cd52778d8462c.

--- backend/server.js
+++ backend/server.js
@@ -11,8 +11,7 @@ dotenv.config();
 const app = express();
 
 // Middleware
-// Middleware
-app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+app.use(cors({ origin: "https://wander-lust-trails-ia8n.vercel.app/", credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
